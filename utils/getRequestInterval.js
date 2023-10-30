d4d7b9cf380c11823734fc4e092e173cea3fdc29
Yunus Gürlek <yunusgurlek@Yunus-MacBook-Pro.local>
2023-10-30 21:53:12 +0100

    -

--- utils/getRequestInterval.js
+++ utils/getRequestInterval.js
@@ -3,5 +3,7 @@ const REQUEST_COUNT_PER_TOKEN_PER_HOUR = 5000;
 const ONE_HOUR_IN_MS = 60 * 60 * 1000;
 
 module.exports = () => {
-  return Math.round(ONE_HOUR_IN_MS / (REQUEST_COUNT_PER_TOKEN_PER_HOUR * API_TOKENS.length));
+  const apiTokenCount = Array.isArray(API_TOKENS) ? API_TOKENS.length : 1;
+
+  return Math.round(ONE_HOUR_IN_MS / (REQUEST_COUNT_PER_TOKEN_PER_HOUR * apiTokenCount));
 }
